72083e5bcc7cc4ead2d0abada3a962b01ded043c
Yangchen <gyachungriom@gmail.com>
2021-06-14 13:21:33 +0200

    till exo 5

---
+++ jour01.js
@@ -0,0 +1,21 @@
+//exo 1
+console.log("Hello World !");
+
+//exo 2
+var test= "My name is Yangchen";
+console.log(test);
+
+//exo 3
+var name="Yangchen";
+console.log("Nice to meet you" + " " + name );
+
+//exo 4
+var testLength= "I'm very long !";
+console.log(testLength.length);
+
+//exo 5
+var food="croissant is meh";
+console.log(food.replace('meh' , 'so good'));
+
+//exo 6
+
