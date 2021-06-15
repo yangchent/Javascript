7152634450bec06aa2a6f46cf2439e92e54338ae
Yangchen <gyachungriom@gmail.com>
2021-06-15 14:09:36 +0200

    exo finished

--- jour02.js
+++ jour02.js
@@ -17,12 +17,48 @@ console.log(rounded);
 //exo4
 var test=12;
 var bis= 5;
-console.log(test + bis);
-console.log(test - bis);
-console.log(test * bis);
-console.log(test/bis);
-console.log(test % bis);
-console.log(test** bis);
+console.log(test + bis);//17
+console.log(test - bis);//7
+console.log(test * bis);//60
+console.log(test/bis);//2.4
+console.log(test % bis);//2
+console.log(test** bis);//248832
 
 //exo5
+var test=143;
+var bis=219;
+console.log(test>bis);//false
+console.log(test<bis);//true
+console.log(test>=bis);//false
+console.log(test<=bis);//true
+console.log(test==bis);//false
+console.log(test===bis);//false
+console.log(test!=bis);//true
+console.log(test!==bis);//true
 
+//exo6
+var limit= 50;
+var score=  64;
+if (score >= limit) { 
+    console.log("ok good")
+}
+else {
+    console.log("Oh nooo...")
+    }
+
+//exo7
+var password= "azerty";
+if (password.length>5) {
+    console.log("The password is secure")
+}
+
+//exo8
+if (score>= limit && password.length>5){
+    console.log("Everything is good")
+}
+else if(score>=limit || password.length>5) {
+    console.log("Something is good")
+}
+else {
+    console.log("Nothing is good")
+}
