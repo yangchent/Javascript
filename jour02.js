8c543c472d0dc0629b307708582b4dc80fce878c
Yangchen <gyachungriom@gmail.com>
2021-06-15 14:56:10 +0200

    exo 3

--- jour02.js
+++ jour02.js
@@ -62,3 +62,45 @@ else if(score>=limit || password.length>5) {
 else {
     console.log("Nothing is good")
 }
+
+//Bonus
+var random= [1, 2, 3, 4, 5, 6];
+if (random===6){
+    console.log("Yes I win")
+}
+else {
+    console.log("So close...")
+}
+
+//Bonus2
+var month= "January";
+switch(month)
+{
+    case "Winter":
+        console.log("c'est l'hiver");
+    break;
+    case "Spring":
+        console.log("c'est printemp");
+    break;
+    case "Summer":
+        console.log("c'est l'été") ;
+    break;
+    case "Fall" :
+        console.log("c'est l'automne");
+    break;       
+    default:
+        console.log("cest n'est pas une season");
+}
+
+//Bonus3
+var roundedNumber=12.4;
+console.log(Math.floor(roundedNumber));//3
+console.log(Math.ceil(roundedNumber));//4
+
+if (roundedNumber>3.5) {
+    console.log(4);
+}
+else {
+    console.log(3);
+}
+//ne march pas avec 12.4 mais d'autre
