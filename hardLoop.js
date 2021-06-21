205e13811228412728de77ddd9f48f2d1e918020
Yangchen <gyachungriom@gmail.com>
2021-06-21 21:38:25 +0200

    till exo 3

--- hardLoop.js
+++ hardLoop.js
@@ -1,8 +1,36 @@
-var numbers =[];
-for (var i = 0; i<= 50; i ++){
-    numbers.push(Math.floor(Math.random(i)*100)  +1);
+// var numbers =[];
+// for (var i = 0; i<= 50; i ++){
+//     numbers.push(Math.floor(Math.random(i)*100)  +1);
+// }
+// for (var i =0; i <= numbers.length; i++){
+//      var total= numbers + i;
+// }
+// console.log(total);
+
+// //Exo 2
+// myNum = [];
+// for (var i =0; i<= 50; i ++){
+//  myNum.push(Math.floor(Math.random(i)*(200-50 + 1)+50));
+// }
+// console.log(myNum);//checking
+//  var maxNum = 0;
+//     for (let i = 1; i < 50; i++) {
+//       if (myNum[i] > maxNum) {
+//         maxNum = myNum[i];
+//       }
+//  }
+//  console.log(maxNum);
+
+ //EXO 3
+ var numbers =[];
+for (var i = 0; i <= 50; i++){
+    numbers.push(Math.floor(Math.random(i)*(200-50 + 1)+50));
 }
-for (var i =0; i <= numbers.length; i++){
-     var total= numbers + i;
+var i = 0;
+while( i <=50){
+    if(numbers[i] >= 75 && numbers[i]<= 100){
+        console.log(numbers[i]);
+    }
+    i++;
 }
-console.log(total);
+//EXO 4
