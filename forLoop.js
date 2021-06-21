a34bd31c55ad0c277d38a8c524f89007d09376f7
Yangchen <gyachungriom@gmail.com>
2021-06-21 13:11:04 +0200

    all is well

--- forLoop.js
+++ forLoop.js
@@ -1,10 +1,15 @@
 // 1
-// var nums = [5, 6, 7, 8, 9, 10];
-// var total = "";
-// for ( var i = 0; i <= nums.length; i ++){
-//     total += Math.sqrt(nums[i]);
-// }
-// console.log(total);
-
-var total = Math.sqrt(5);
+var total =0;
+for ( var i = 5; i <= 10; i ++){
+    total += i * i;
+}
 console.log(total);
+
+// 2
+var sum= 0;
+for ( i = 100; i <= 1000; i ++){
+    sum += i% 7 === 0;
+}
+console.log(sum);
+
+// 3
