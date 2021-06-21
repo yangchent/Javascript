3fdcaa8d856cc1ad9ec030ab1f1af17ac82ae735
Yangchen <gyachungriom@gmail.com>
2021-06-21 16:49:37 +0200

    till exo 5

--- whileLoop.js
+++ whileLoop.js
@@ -1,23 +1,23 @@
-// // 1
-// var i = 50;
-// while (i <= 200){
-//     if (i % 2 === 0){
-//         console.log(i);
-//     }
-//  i++;
-// }
+// 1
+var i = 50;
+while (i <= 200){
+    if (i % 2 === 0){
+        console.log(i);
+    }
+ i++;
+}
 
-// // 2
-// var dice= null,
-//     count= 0;
-// function random(){
-//     return Math.floor(Math.random() *6);
-// }    
-// while (dice !==6){
-//     count= random() + 1;
-//     dice++;
-// }
-// console.log(count);
+// 2
+var dice= null,
+    count= 0;
+function random(){
+    return Math.floor(Math.random() *6);
+}    
+while (dice !==6){
+    count= random() + 1;
+    dice++;
+}
+console.log(count);
 
 // 3
 var ussainBolt = 0,
@@ -36,3 +36,14 @@ while (ussainBolt <=100 || tysonGay <=100){
     ussainBolt++;
 tysonGay++;
 }
+//Exo 4
+//5
+//2
+//5
+//-8
+
+//Exo 5
+//8
+//9
+//10
+//11
