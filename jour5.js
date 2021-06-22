7c4a79ea87913afef5048eede5cf4a6881d80772
Yangchen <gyachungriom@gmail.com>
2021-06-22 17:15:12 +0200

    correction

--- jour5.js
+++ jour5.js
@@ -14,8 +14,12 @@ function calculator(num1,operateur,num2){
         console.log(num1 % num2);
     }
 }
-// calculator(4 ,"*", 5);
-// console.log(process.argv);
- calculator(parseInt(process.argv[2]),process.argv[3],parseInt(process.argv[4]));
+if (process.argv.length === 5){
+    calculator(parseInt(process.argv[2]),process.argv[3],parseInt(process.argv[4]));
+}
+else {
+    console.log("Error");
+}
 
- //EXO 3
+// calculator(4 ,"*", 5);
+// console.log(process.argv)
