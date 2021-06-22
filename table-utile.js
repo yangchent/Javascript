82dedc705d121a9e3873c96168369dc1d6fe4478
Yangchen <gyachungriom@gmail.com>
2021-06-22 17:01:04 +0200

    not exported yet

---
+++ table-utile.js
@@ -0,0 +1,33 @@
+
+ //Exo 2 Multiply
+ function multiply(num){
+    for (var i = 1; i <= 10; i++){
+     var  multi = num * i;
+     console.log(`${num} * ${i}= ${multi}`);
+    }
+  }
+ //  console.log(process.argv);
+ //  multiply(6);
+ if (process.argv.length === 3){
+     multiply(process.argv[2]);
+ }
+ else {
+     console.log("Error");
+ }
+
+ //Addition
+ function addition(num){
+    for (var i = 1; i <= 10; i++){
+     var  add = num + i;
+     console.log(`${num} + ${i}= ${add}`);
+    }
+  }
+ //  console.log(process.argv);
+ //  multiply(6);
+ if (process.argv.length === 3){
+     addition(parseInt(process.argv[2]));
+ }
+ else {
+     console.log("Error");
+ }
+ 
