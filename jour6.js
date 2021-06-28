9796075c0e4657d1fce5ee7ddb6a9794823a135d
Yangchen <gyachungriom@gmail.com>
2021-06-28 14:12:42 +0200

    to be finish after

--- jour6.js
+++ jour6.js
@@ -1,63 +1,71 @@
-//EXO 1 FORMAT
+// //EXO 1 FORMAT
 
-function formatDate(a){
+// function formatDate(a){
 
-     a = new Date(a);
-    const date = a.getDate();
-    const monthName = a.getMonth()+1;
-    const year = a.getFullYear();
+//      a = new Date(a);
+//     const date = a.getDate();
+//     const monthName = a.getMonth();
+//     const year = a.getFullYear();
 
-    console.log(date + '/' + monthName + '/' + year);
-}
-formatDate("2021/03/11");
+//     console.log(date + '/' + monthName +1 + '/' + year);
+// }
+// formatDate("2021/03/11");
 
-//Exo 2 AGE
-function calculateAge(str){
+// //correction exo 1
+// function formatDate(string){
+//   let date= new Date(string);
+//  console.log(`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`)
 
-    var a = new Date(str);
-    console.log(a)
+// }
+// formatDate("2001/10/17");
 
-    console.log(new Date())
+// //Exo 2 AGE
+// function calculateAge(str){
+
+//     var a = new Date(str);
+//     console.log(a)
+
+//     console.log(new Date())
     
-    var year = new Date(new 
-        Date -a); 
-    console.log(year);
-    console.log(year.getFullYear()-1970)
-}
-calculateAge("1985-11-24");
+//     var year = new Date(new 
+//         Date -a); 
+//     console.log(year);
+//     console.log(year.getFullYear()-1970)
+// }
+// calculateAge("1985-11-24");
 
-//Exo 2 AGE Simple way
-function calculateAge(str){
+// //Exo 2 AGE Simple way
+// function calculateAge(str){
      
-    var a = new Date(),
-       yearNow= a.getFullYear(),
-       monthNow= a.getMonth()+1,
-       dateNow= a.getDate();
+//     var a = new Date(),
+//        yearNow= a.getFullYear(),
+//        monthNow= a.getMonth()+1,
+//        dateNow= a.getDate();
 
-    var b= new Date(str),
-        yearB= b.getFullYear(str),
-        monthB= b.getMonth(str)+1,
-        dateB= b.getDate(str);
+//     var b= new Date(str),
+//         yearB= b.getFullYear(str),
+//         monthB= b.getMonth(str)+1,
+//         dateB= b.getDate(str);
 
-      var age= 0;  
-    if(monthNow > monthB){
-      age =yearNow- yearB;
-    } 
-    else if(monthNow == monthB){
+//       var age= 0;  
+//     if(monthNow > monthB){
+//       age =yearNow- yearB;
+//     } 
+//     else if(monthNow == monthB){
      
-      if(dateNow>= dateB){
-        age = yearNow - yearB;
-      }
-      else {
-        age = yearNow - yearB - 1;
-      }
-    }
-    else {
-      age = yearNow- yearB -1;
-    }
-    console.log(age) ;
-}
-calculateAge("1985-11-24");
+//       if(dateNow>= dateB){
+//         age = yearNow - yearB;
+//       }
+//       else {
+//         age = yearNow - yearB - 1;
+//       }
+//     }
+//     else {
+//       age = yearNow- yearB -1;
+//     }
+//     console.log(age) ;
+// }
+// calculateAge("1985-11-24");
 
 //Exo 3 VALIDATOR
 
@@ -65,10 +73,8 @@ var prompt = require("prompt");
 
 prompt.start();  // démarre le prompt
 
-function onErr(err) { // permet de gérer les erreurs
-  console.log(err);
-  return;
-}
+function checkProfile() { 
+
 
 prompt.get([{
     name : "Username", 
@@ -84,17 +90,17 @@ prompt.get([{
     name : "password",
     description : "Password",
     validator : /[a-z@A-Z1-9-_]/gi
+},
+
+function (err, res) { 
+  
+  if (res.name ==/^[a-zA-Z\s\-]+$/g && res.email ==/[a-z@A-Z1-9-_]/gi && res.password == /[a-z@A-Z1-9-_]/g){
+  console.log("All good ! ");
+ }
+  else {
+    console.log("error");
+  }
+}]);
 }
-]
-    , 
-    function (err, res) {  // demande à obtenir les propriétés "username" et "email"
-  if (err) {
-		return onErr(err);
-	}
 
-  console.log("Données reçues :");
-  console.log("=> Username : " + res.username); // affiche le résultat pour la propriété "username"
-  console.log("=> Email : " + res.email); // affiche le résultat pour la propriété "email"
-});
-
-
+checkProfile();
