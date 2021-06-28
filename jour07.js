7392b43abca4ed062ba4d180016ef360f225a2e2
Yangchen <gyachungriom@gmail.com>
2021-06-28 14:37:36 +0200

    exo 3

--- jour07.js
+++ jour07.js
@@ -1,8 +1,30 @@
-const fs = require("fs");
-fs.readFile("jour07.txt" , function (err,data) {
-    if (err){
-        console.log("error");
-        return;
-    }
-    console.log(data.toString());
-});
+// // EXO 1
+// const fs = require("fs");
+// fs.readFile("jour07.txt" , function (err,data) {
+//     if (err){
+//         console.log("error");
+//         return;
+//     }
+//     console.log(data.toString());
+// });
+// // EXO 2
+// var array=[1, 2, 3, 4, 5];
+// var double= array.map(num=> num *2);
+// console.log(double);
+
+//EXO 3
+var longNames = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+];
+var shortNames = longNames.map(function (name){
+    return name.firstName + name.lastName;
+
+}  );
+console.log(shortNames);
