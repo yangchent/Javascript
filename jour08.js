bd883f1f61ec713fa9615416b535161f0411bb89
Yangchen <gyachungriom@gmail.com>
2021-06-29 15:37:52 +0200

    till exo 2

--- jour08.js
+++ jour08.js
@@ -1,10 +1,23 @@
+// //EXO 1 COUNTRIES
 const axios= require('axios');
 
 
 axios.get("https://restcountries.eu/rest/v2/all").then((res)=> {
+    
+    
     var getCountries = res.data;
-    const countriesName = getCountries.map(getCountries => getCountries.name);
-     console.log(countriesName);
+    const countriesName = [];
+    countriesName.push(getCountries.map(getCountries => getCountries.name +'-'))  ;
+     console.log(`${countriesName}`);
 }
 );
+//EXO 2 CHUCK NORRIS
+
+function getFact(){
+axios.get("https://api.chucknorris.io/jokes/random").then ((res) => {
+    var jokes= res.data;
+    console.log(jokes.value);
+})
+}
+getFact();
 
