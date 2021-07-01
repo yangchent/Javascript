906d1039f3e9a450190b9d645507d9eb33af39f7
Yangchen <gyachungriom@gmail.com>
2021-07-01 17:02:10 +0200

    Project rover finished Bonus!Finished

--- projectRover.js
+++ projectRover.js
@@ -17,32 +17,28 @@ var rover= {
 	direction :"N",
 	x:0,
 	y:0,
-	travelLog:["x","y"],
+	travelLog:[],
 };
 
 function turnLeft(rover){
 	
     if(rover.direction ==="N"){
         rover.direction ="W";
-		// console.log(rover + "this is west")
          }
 	else if(rover.direction ==="W"){
         rover.direction ="S";
-		// console.log(rover + "this is south")
 	}
 	else if  (rover.direction ==="S"){
         rover.direction ="E";
-		// console.log(rover + "this is east")
 	}
 	else if(rover.direction ==="E") {
 		rover.direction ="N";
-		// console.log(rover + "this is north")
 	}
 	 console.log(rover);
-	 grid[rover.x][rover.y]=rover.direction
+	 grid[rover.y][rover.x]=rover.direction
 
 }
-turnLeft(rover);
+ turnLeft(rover);
 
 
 
@@ -50,100 +46,139 @@ function turnRight(rover){
 	
     if(rover.direction ==="N"){
         rover.direction ="E";
-		// console.log(rover + "this is east")
          }
 	else if(rover.direction ==="E"){
         rover.direction ="S";
-		// console.log(rover + "this is south")
 	}
 	else if  (rover.direction ==="S"){
         rover.direction ="W";
-		// console.log(rover + "this is west")
 	}
 	else if(rover.direction ==="W") {
 		rover.direction ="N";
-		// console.log(rover + "this is north")
 	}
 	 console.log(rover);
-	  grid[rover.y][rover.x]= rover.direction
+	  grid[rover.y][rover.x]=rover.direction
   
 };
 
 function moveForward(rover){
-	if((rover.x<0||rover.x>10)||(rover.y<0||rover.y>10)){
+	if(rover.x<0||rover.x>9||rover.y<0||rover.y>9){
+
 			console.log("go other direction")
 	}
-	else if(rover.direction==="N"){					
-		rover.y--;						
-	}
-	else if(rover.direction==="E"){		
-		rover.x--;			
-	}
-	else if(rover.direction==="S"){
-		rover.y++;	
-	}
-	else if(rover.direction==="W"){
-		rover.x++;	
-	}
-	console.log(rover)
-}
-
-function moveBackward(rover){
-	if((rover.x<0||rover.x>10)||(rover.y<0||rover.y>10)){
-		console.log("go other direction")
-	}
 	else if(rover.direction==="N"){
-		rover.y++;	
+		if(rover.y!=0){
+			rover.y--;	
+		}
 	}
 	else if(rover.direction==="E"){
-		rover.x++;	
+		if(rover.x!=9){
+			rover.x++;	
+		}
 	}
 	else if(rover.direction==="S"){
-		rover.y--;	
+		if(rover.y!=9){
+			rover.y++;
+		}
 	}
 	else if(rover.direction==="W"){
-		rover.x--;	
+		if(rover.x!=0){
+			rover.x--;	
+		}
 	}
 	console.log(rover)
+	grid[rover.y][rover.x]=rover.direction
 }
-moveForward(rover);
-moveBackward(rover);
+
+// function moveBackward(rover){
+// 	if(rover.x<9||rover.x>0||rover.y<9||rover.y>0){
+
+// 			console.log("go other direction")
+// 	}
+// 	else if(rover.direction==="N"){
+// 		if(rover.y!=9){
+// 			rover.y++;	
+// 		}
+// 	}
+// 	else if(rover.direction==="E"){
+// 		if(rover.x!=0){
+// 			rover.x--;	
+// 		}
+// 	}
+// 	else if(rover.direction==="S"){
+// 		if(rover.y!=0){
+// 			rover.y--;
+// 		}
+// 	}
+// 	else if(rover.direction==="W"){
+// 		if(rover.x!=9){
+// 			rover.x++;	
+// 		}
+// 	}
+// 	console.log(rover)
+// 	grid[rover.y][rover.x]=rover.direction
+// }
 
 function pilotRover(string){
 	 
  for ( var i =0; i <= string.length; i ++ ){
 		if (string[i]==="l"){
 			turnLeft(rover)
-			rover.travelLog.push([rover.x, rover.y]);
+			rover.travelLog.push([rover.x,rover.y])
 		}
 		else if(string[i]==="r"){
 			turnRight(rover)
-			rover.travelLog.push([rover.x, rover.y]);
+			rover.travelLog.push([rover.x,rover.y])
 		}
 		else if(string[i]==="f"){
 			moveForward(rover)
-			rover.travelLog.push([rover.x, rover.y]);
+			rover.travelLog.push([rover.x,rover.y])
 		}
 
-		else if(string[i]==="b"){
-			moveBackward(rover)
-			rover.travelLog.push([rover.x, rover.y]);
-		}
+		// else if(string[i]==="b"){
+		// 	moveBackward(rover)
+		// 	rover.travelLog.push([rover.x,rover.y])
+			
+		// }
  }
  console.table(grid)
 }
 
-pilotRover("rrllffbbm")
+// pilotRover("rrllffbbm")
 
 function show (){
-	prompt.get( [show], function (err,res){
+	prompt.get({name:"input",
+		description: "give me r/l/f/b",
+	}, 
+	function (err,res){
 		if (err) {
-			return onErr(err);
+			return("errr");
 		}
-        else {
-	  console.log(res.show);
-	  pilotRover("rrllffbbm")
-        }
+		else if(res.input==="l"){
+			pilotRover("l")
+			show()
+		}
+		else if(res.input==="r"){
+			pilotRover("r")
+			show()
+		}
+		else if(res.input==="f"){
+			pilotRover("f")
+			show()
+		}
+		else if(res.input=== "b"){
+			pilotRover("b")
+			show()
+		  }
+		
+	//   console.log(res.show);
+	//   pilotRover("rrllffbbm")
+
+	//   console.log(rover.travelLog)
+	  
+
+	
+	   
 	})
 }
 show()
